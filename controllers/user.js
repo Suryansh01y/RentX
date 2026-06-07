5d19f8790f7d4d0dabb9a2a58b731d98cb4234c5
Suryansh01y <suryanshyadavv01@gmail.com>
2026-06-07 21:40:50 +0530

    Made some UI changes and some updation in Names

--- controllers/user.js
+++ controllers/user.js
@@ -15,7 +15,7 @@ module.exports.signUp = async (req, res) =>
        if(err){ 
         return next(err);
        }
-       req.flash("success", "Welcome to Wanderlust");
+       req.flash("success", "Welcome to RentX");
        res.redirect("/listings");
     });
     }catch(e){
@@ -29,7 +29,7 @@ module.exports.signUp = async (req, res) =>
 };
 
 module.exports.login =   async(req, res) => {
-       req.flash("success", "Welcome back to Wanderlust") ;
+       req.flash("success", "Welcome back to RentX") ;
        let redirectUrl = res.locals.redirectUrl || "/listings" ;
        res.redirect(redirectUrl);
 };
